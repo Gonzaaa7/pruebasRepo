ccd616736a7804544cfbda1365bb87de2dfa8969
Gonzalo <gonzalonicolasblanco7@gmail.com>
2025-08-22 16:22:07 -0300

    versionx

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const ArmaPrincipal = ["Bastarda Blanca","Hacha Nordica","Scimitarra","Clava de Guerra"];
+const Escudos = ["Escudo Nordico","E"];
+const Arrojadiza = ["Dardos","Javalinas","Hacha Arrojadiza"];
+const ArmaSecundaria = ["Lanza Comun","HowlPicke","Lanza de Guerra","Martillo"];
+
